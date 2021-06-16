21d65af885d8daa743da7cfc5c4bcece9fff948f
Vincent <Vincent.Krenzke@gmx.de>
2021-06-17 00:15:18 +0200

    extracted unloading task class

--- Assets/Scripts/Task.cs
+++ Assets/Scripts/Task.cs
@@ -1,49 +1,8 @@
-﻿using UnityEngine.AI;
-
+﻿
 public abstract class Task
 {
     public TaskDataUnloadingSO TaskDataUnloading { get; protected set; }
     public float CurrentTaskGoalAmount { get; protected set; }
     public abstract void IncreaseCurrentAmount();
     public abstract void StartTask();
-}
-
-public class UnloadingTask : Task
-{
-    private VehicleController _vehicleController;
-
-    public UnloadingTask(TaskDataBaseSO taskData)
-    {
-        TaskDataUnloading = (TaskDataUnloadingSO) taskData;
-        TaskDataUnloading.CargoUnloadedEventChannel.OnCargoUnload += OnCargoUnloadedUnloaded;
-    }
-
-    private void OnCargoUnloadedUnloaded(VehicleController vehicleController)
-    {
-        if (vehicleController == this._vehicleController)
-        {
-            IncreaseCurrentAmount();
-        }
-    }
-
-    public override void IncreaseCurrentAmount()
-    {
-        CurrentTaskGoalAmount++;
-        TaskDataUnloading.TaskProgressionEventChannel.RaiseEvent(this);
-        if (CurrentTaskGoalAmount >= TaskDataUnloading.RequiredAmount)
-        {
-            TaskDataUnloading.TaskCompletedEventChannel.RaiseEvent(this);
-        }
-    }
-
-    public override void StartTask()
-    {
-        TaskDataUnloading.InstantiateVehicle();
-        _vehicleController = TaskDataUnloading.Vehicle.GetComponent<VehicleController>();
-
-        var vehicle = TaskDataUnloading.Vehicle;
-        var navAgent = vehicle.GetComponent<NavMeshAgent>();
-        navAgent.enabled = true;
-        navAgent.destination = TaskDataUnloading.UnloadTargetPosition.position;
-    }
 }
---
+++ Assets/Scripts/UnloadingTask.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine.AI;
+
+public class UnloadingTask : Task
+{
+    private VehicleController _vehicleController;
+
+    public UnloadingTask(TaskDataBaseSO taskData)
+    {
+        TaskDataUnloading = (TaskDataUnloadingSO) taskData;
+        TaskDataUnloading.CargoUnloadedEventChannel.OnCargoUnload += OnCargoUnloadedUnloaded;
+    }
+
+    private void OnCargoUnloadedUnloaded(VehicleController vehicleController)
+    {
+        if (vehicleController == this._vehicleController)
+        {
+            IncreaseCurrentAmount();
+        }
+    }
+
+    public override void IncreaseCurrentAmount()
+    {
+        CurrentTaskGoalAmount++;
+        TaskDataUnloading.TaskProgressionEventChannel.RaiseEvent(this);
+        if (CurrentTaskGoalAmount >= TaskDataUnloading.RequiredAmount)
+        {
+            TaskDataUnloading.TaskCompletedEventChannel.RaiseEvent(this);
+        }
+    }
+
+    public override void StartTask()
+    {
+        TaskDataUnloading.InstantiateVehicle();
+        _vehicleController = TaskDataUnloading.Vehicle.GetComponent<VehicleController>();
+
+        var vehicle = TaskDataUnloading.Vehicle;
+        var navAgent = vehicle.GetComponent<NavMeshAgent>();
+        navAgent.enabled = true;
+        navAgent.destination = TaskDataUnloading.UnloadTargetPosition.position;
+    }
+}
