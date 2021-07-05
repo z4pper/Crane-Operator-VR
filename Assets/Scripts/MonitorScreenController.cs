98b1000d5a954dfe41ef68da7921a8d8f9968ba8
Vincent <Vincent.Krenzke@gmx.de>
2021-07-05 15:20:06 +0200

    implemented monitor screen tutorial

---
+++ Assets/Scripts/MonitorScreenController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class MonitorScreenController : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> monitorScreensInOrder = new List<GameObject>();
+
+    private int _currentIndex;
+
+    private void Start()
+    {
+        monitorScreensInOrder.ForEach(screen => screen.SetActive(false));
+        _currentIndex = 0;
+        monitorScreensInOrder[_currentIndex].SetActive(true);
+    }
+
+    public void NextScreen()
+    {
+        monitorScreensInOrder[_currentIndex].SetActive(false);
+        
+        _currentIndex = (_currentIndex + 1) % monitorScreensInOrder.Count;
+        
+        monitorScreensInOrder[_currentIndex].SetActive(true);
+    }
+}
---
+++ Assets/Scripts/TouchScreenButton.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(BoxCollider))]
+public class TouchScreenButton : MonoBehaviour
+{
+    [SerializeField] private UnityEvent OnTouchScreen;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        OnTouchScreen?.Invoke();
+    }
+}
